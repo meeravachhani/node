9a0ad5f8fb1d0f01ed4cd5c5884539e829cb19d6
meeravachhani <meeravachhani02@gmail.com>
2025-10-04 12:27:45 +0530

    Delete sequelize directory

--- sequelize/config.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize("mm", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
--- sequelize/validations/songValidation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const songSchema = Joi.object({
-  title: Joi.string().required(),
-  category: Joi.string().required(),
-  url: Joi.string().uri().required(),
-});
-
-module.exports = { songSchema };
