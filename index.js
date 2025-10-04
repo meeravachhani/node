09d6f9918066c8ddce22c697206e86d6bd39d3f2
meeravachhani <meeravachhani02@gmail.com>
2025-10-04 12:56:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
-app.use("/", studentRoute);
+app.get('/', (req, res) => {
+    res.send("welcome to mca");
+});
+
 app.listen(3000, () => {
-  console.log("server is running : 127.0.0.1");
+
+    console.log("server is running : 127.0.0.1:3000");
 
 });
